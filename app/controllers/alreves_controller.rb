2b48d1ae369cb3ee2e97251e7164b9dadcf792cc
buzz <buzz@users.noreply.github.com>
2008-11-08 20:29:11 +0100

    added boot action to AlrevesController

--- app/controllers/alreves_controller.rb
+++ app/controllers/alreves_controller.rb
@@ -1,4 +1,10 @@
 class AlrevesController < ActionController::Base
+  # starts the web application using rhtml
+  def alreves_boot
+    render
+  end
+
+  # send default component (usually page layout)
   def alreves_init
     @components.push(Component.new())
     render :text => get_response_json
--- routes.rb
+++ routes.rb
@@ -1,6 +1,8 @@
+# boot the alreves client in the user's browser
+connect '/', :controller => 'application', :action => 'alreves_boot', :conditions => { :method => :get }
+# initial action after boot
 connect 'alreves_init', :controller => 'application', :action => 'alreves_init', :conditions => { :method => :post }
 connect ':controller', :action => 'index', :conditions => { :method => :post }
 connect ':controller/:id', :action => 'show', :id => /\d+/, :conditions => { :method => :post }
 connect ':controller/:action', :conditions => { :method => :post }
 connect ':controller/:action/:id', :conditions => { :method => :post }
-
