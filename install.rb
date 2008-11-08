450308fec22eb7c1869ef3f32f0b169bc60b9742
buzz <buzz@users.noreply.github.com>
2008-11-08 20:57:48 +0100

    removed copy of index.html since the boot procedure is provided via rhtml now

--- install.rb
+++ install.rb
@@ -1,8 +1,4 @@
 dir = File.join(RAILS_ROOT, 'public', 'templates')
 puts "Create directory '#{dir}'"
 FileUtils.mkdir(dir)
-src = File.join(File.dirname(__FILE__), 'assets', 'index.html')
-dest = File.join(RAILS_ROOT, 'public')
-puts "Copy '#{src}' to '#{dest}'"
-FileUtils.cp(src, dest)
 
