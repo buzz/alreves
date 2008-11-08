87e6a7641a928da6ffaa831662af07bb619122ce
buzz <buzz@users.noreply.github.com>
2008-11-08 20:40:52 +0100

    authenticity token added to boot page, indentation in jquery.alreves.js, javascript asset include :alreves

--- assets/javascripts/jquery.alreves.js
+++ assets/javascripts/jquery.alreves.js
@@ -15,66 +15,72 @@ jQuery.alreves = {
 
   /* engine caches */
   caches: {
-		templates: {},
-		macros: {}
+    templates: {},
+    macros: {}
   },
+
+  /* authenticity_token for Rail's RequestForgeryProtection */
+  authenticity_token: '',
 
   /* update page with content */
   updatePage: function(components) {
-	if (this.request_count < 1) {
-	  jQuery.each(components, function(key, component) {
-			// for storing macros
-			component.data.macros = {}
-			// apply common macros
+  if (this.request_count < 1) {
+    jQuery.each(components, function(key, component) {
+      // for storing macros
+      component.data.macros = {}
+      // apply common macros
       jQuery.each(jQuery.alreves.caches.macros, function(key, template) {
-				template.process(component.data);
-			});
-		  jQuery(component.dest).html(jQuery.alreves.caches.templates[component.template_name].process(component.data));
-		  jQuery(component.dest).fadeIn('slow');
-		});
-	}
+        template.process(component.data);
+      });
+      jQuery(component.dest).html(jQuery.alreves.caches.templates[component.template_name].process(component.data));
+    });
+  }
   },
 
   /* process server response */
   processResponse: function(components) {
-	if (typeof components == 'object')
-	  jQuery.each(components, function(key, component) {
-		  if (typeof component.template_name != 'undefined' && typeof component.dest != 'undefined') {
-			// download template if not already cached
-			if (typeof jQuery.alreves.caches.templates[component.template_name] == 'undefined') {
-			  ++jQuery.alreves.request_count;
-			  jQuery.ajax({
-				  type: "GET",
-					url: 'templates/'+component.template_name+'.tpl',
-					success: function(response) {
-						jQuery.alreves.caches.templates[component.template_name] = TrimPath.parseTemplate(response);
-						--jQuery.alreves.request_count;
-						jQuery.alreves.updatePage(components);
-				  },
-					error: function(XMLHttpRequest, textStatus, errorThrown) {
-						console.log('AJAX request error: ' + textStatus + ' (' + errorThrown + ')');
-						--jQuery.alreves.request_count;
-				  }
-				});
-			} else
-			  jQuery.alreves.updatePage(components);
-		  }
-		});
+  if (typeof components == 'object')
+    jQuery.each(components, function(key, component) {
+      if (typeof component.template_name != 'undefined' && typeof component.dest != 'undefined') {
+        // download template if not already cached
+        if (typeof jQuery.alreves.caches.templates[component.template_name] == 'undefined') {
+          ++jQuery.alreves.request_count;
+          jQuery.ajax({
+            type: "GET",
+            url: 'templates/'+component.template_name+'.tpl',
+            success: function(response) {
+              jQuery.alreves.caches.templates[component.template_name] = TrimPath.parseTemplate(response);
+              --jQuery.alreves.request_count;
+              jQuery.alreves.updatePage(components);
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+              console.log('AJAX request error: ' + textStatus + ' (' + errorThrown + ')');
+              --jQuery.alreves.request_count;
+            }
+          });
+        } else
+          jQuery.alreves.updatePage(components);
+      }
+    });
   },
 
   /* execute an action on server */
   loadURL: function(url, post_data) {
-	if(!this.processing_state)
-	  jQuery.post(url, typeof post_data == 'object' ? post_data : null, function(response) {
-		  jQuery.alreves.processResponse(response);
-		}, 'json');
+  if (!this.processing_state)
+    if (typeof post_data != 'object') {
+      post_data = {};
+    }
+    post_data.authenticity_token = this.authenticity_token;
+    jQuery.post(url, post_data, function(response) {
+      jQuery.alreves.processResponse(response);
+    }, 'json');
   },
 
   /* Submit a form using post ajax request */
   submitForm: function(form) {
-		var fields = jQuery('#'+form+' :input').serializeArray();
-		this.loadURL(jQuery('#'+form).attr('action'), fields);
-		jQuery('#'+form+' :input').disable();
+    var fields = jQuery('#'+form+' :input').serializeArray();
+    this.loadURL(jQuery('#'+form).attr('action'), fields);
+    jQuery('#'+form+' :input').disable();
   },
 
   /* Inject stylesheet */
@@ -84,18 +90,18 @@ jQuery.alreves = {
 
   /* Add macro file */
   addMacroFile: function(macro_url) {
-		if (typeof this.caches.macros[macro_url] == 'undefined') {
-			jQuery.ajax({
-				type: "GET",
-				url: macro_url,
-				success: function(response) {
-					jQuery.alreves.caches.macros[macro_url] = TrimPath.parseTemplate(response);
-				},
-				error: function(XMLHttpRequest, textStatus, errorThrown) {
-					console.log('AJAX request error: ' + textStatus + ' (' + errorThrown + ')');
-				}
-			});
-		}
-	}
+    if (typeof this.caches.macros[macro_url] == 'undefined') {
+      jQuery.ajax({
+        type: "GET",
+        url: macro_url,
+        success: function(response) {
+          jQuery.alreves.caches.macros[macro_url] = TrimPath.parseTemplate(response);
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+          console.log('AJAX request error: ' + textStatus + ' (' + errorThrown + ')');
+        }
+      });
+    }
+  }
 }
 
--- init.rb
+++ init.rb
@@ -1,2 +1,3 @@
 ActionController::Base.send :include, AlrevesControllerHelper
-
+ActionView::Helpers::AssetTagHelper::register_javascript_expansion :alreves => ["../plugin_assets/alreves/javascripts/jquery.alreves.js",
+                                                                                "../plugin_assets/alreves/javascripts/trimpath-template.js"]
