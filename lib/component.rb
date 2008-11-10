c1b1e69511b54a431994ba9f56a4d16eb373ec81
buzz <buzz@users.noreply.github.com>
2008-11-10 17:41:27 +0100

    better json_opts handling for Hashes, Arrays and ActiveRecords

--- lib/component.rb
+++ lib/component.rb
@@ -10,10 +10,16 @@ class Component
   def to_json
     data = []
     @data.each do |key, value|
-      if value.is_a? ActiveRecord::Base
-        data.push "'#{key}':#{value[:data].to_json(value[:json_opts])}"
+      if value.is_a? Hash and !value[:data].nil?
+        if !value[:json_opts].nil? and
+            value[:data].is_a? ActiveRecord::Base or
+            value[:data].is_a? Array
+          data.push "'#{key}':#{value[:data].to_json value[:json_opts]}"
+        else
+          data.push "'#{key}':#{value[:data].to_json}"
+        end
       else
-        data.push "'#{key}':#{value[:data].to_json()}"
+        data.push "'#{key}':#{value.to_json}"
       end
     end
     "{'dest':'#{@dest}','template_name':'#{@template_name}','data':{#{data.join(',')}}}"
